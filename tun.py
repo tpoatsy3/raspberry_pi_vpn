da35f6ae45e8b786680ede25ac47d46d8c77649d
Ted Poatsy <tedpoatsy3@gmail.com>
2017-05-29 19:28:04 -0400

    attempting

---
+++ tun.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python
+
+#
+#  Fake ICMP and ARP responses from non-existings IPs via tap0.
+#   Create fake MAC addresses on the fly.
+#
+
+from scapy.all import *
+
+import os
+import subprocess
+import socket
+
+import pytap    # my pytab wrapper around basic system-specific syscalls
+
+tun, ifname = pytap.open('tun0')
+print "Allocated interface %s. Configuring it." % ifname
+
+subprocess.check_call("ifconfig %s 10.5.0.100 up" % ifname, shell=True)
+subprocess.check_call("route add -net 10.5.0.0 netmask 255.255.255.0 dev %s" % ifname, shell=True)
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.IPPROTO_UDP)
+#sock.connect(('129.170.239.178', 18958))
+sock.bind(('192.168.56.100', 2003))
+
+
+
+
+#  Now process packets
+while 1:
+
+    binary_packet = os.read(tun, 2048)   # get packet routed to our "network"
+
+    # The packet may be IPv4 or IPv6.
+    #   Parsing IPv6 as IPv4 will give strange results, so check which we got.
+    if ord(binary_packet[0]) == 0x60 :
+        packet = IPv6(binary_packet)  # Scapy parses byte string into a packet object
+    else:
+        packet = IP(binary_packet)
+
+
+
+
+    # Send packet to VPN server over socket  using write() with encapsulation
+    packet_wrapped = IP(src='10.5.0.100', dst='129.170.239.178')/UDP(sport=2003, dport=18958)/Raw(binary_packet)
+
+    # packet_wrapped.version  = "4L"
+    # packet_wrapped.ihl = "5L"
+    del packet_wrapped[IP].chksum
+    packet_wrapped = packet_wrapped.__class__(str(packet_wrapped))
+    sendp(packet_wrapped, iface="eth0")
+
+
+
+    packet_wrapped.show()
+    #os.write(sock.fileno(), str(packet_wrapped))
+
+
+    # tell VPN server that I am 10.5.0.100 so it gives me all the packets for that addresses
+    # include IP in pull request to server. Have to write that protocol.
+    # os.write() that packet to tun interface
+
+    print packet.summary()
+    print hexdump(packet)
+
+
+    # Server:
+    # dictionary/hashtable of received packets
