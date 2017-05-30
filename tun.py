f3c8720ff496e55c5a889bf8164c7b034499fe1a
Ted Poatsy <tedpoatsy3@gmail.com>
2017-05-29 21:55:54 -0400

    client almost done

--- tun.py
+++ tun.py
@@ -20,8 +20,13 @@ subprocess.check_call("ifconfig %s 10.5.0.100 up" % ifname, shell=True)
 subprocess.check_call("route add -net 10.5.0.0 netmask 255.255.255.0 dev %s" % ifname, shell=True)
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.IPPROTO_UDP)
-#sock.connect(('129.170.239.178', 18958))
-sock.bind(('192.168.56.100', 2003))
+sock.setsockopt(socket.IPPROTO_IP, socket.IP_HDRINCL, 1)
+sock.connect(('129.170.239.178', 18958))
+
+sock2 = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.IPPROTO_UDP)
+sock2.setsockopt(socket.IPPROTO_IP, socket.IP_HDRINCL, 1)
+sock2.settimeout(.1)
+sock2.connect(('129.170.239.178', 18959))
 
 
 
@@ -42,26 +47,34 @@ while 1:
 
 
     # Send packet to VPN server over socket  using write() with encapsulation
-    packet_wrapped = IP(src='10.5.0.100', dst='129.170.239.178')/UDP(sport=2003, dport=18958)/Raw(binary_packet)
+    packet_wrapped = IP(src='192.168.56.100', dst='129.170.239.178')/UDP(sport=2003, dport=18958)/packet
 
     # packet_wrapped.version  = "4L"
     # packet_wrapped.ihl = "5L"
     del packet_wrapped[IP].chksum
     packet_wrapped = packet_wrapped.__class__(str(packet_wrapped))
-    sendp(packet_wrapped, iface="eth0")
 
 
-
-    packet_wrapped.show()
-    #os.write(sock.fileno(), str(packet_wrapped))
+    # packet_wrapped.show()
+    sock.sendto(str(packet_wrapped),('129.170.239.178', 18958))
 
 
     # tell VPN server that I am 10.5.0.100 so it gives me all the packets for that addresses
     # include IP in pull request to server. Have to write that protocol.
-    # os.write() that packet to tun interface
+    rec_request_pkt = IP(src="192.168.56.100", dst="129.170.239.178")/UDP(sport=6060,dport=18959)/Raw("10.5.0.100")
+    sock2.sendto(str(rec_request_pkt),('129.170.239.178', 18959))
 
-    print packet.summary()
-    print hexdump(packet)
+    buff = ''
+    try:
+        buff, address = sock2.recvfrom(65000)
+        # Strip the outer header off
+    except:
+        continue
+
+    os.write(tun, buff)
+
+    # print packet.summary()
+    # print hexdump(packet)
 
 
     # Server:
